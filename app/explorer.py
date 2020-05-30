7a69e8850db33600aed999d8125db5c12f4d111a
Chris Cummins <chrisc.101@gmail.com>
2020-05-30 16:09:52 +0100

    Switch to clang default IR.
    Signed-off-by: format 20.02.05 <github.com/ChrisCummins/format>

--- app/explorer.py
+++ app/explorer.py
@@ -6,28 +6,15 @@ from app import explorer
 
 # The default IR to display.
 DEFAULT_IR = """\
-source_filename = "example.c"
-
-define i32 @Fib(i32) local_unnamed_addr #0 {
-  switch i32 %0, label %3 [
-    i32 0, label %9
-    i32 1, label %2
-  ]
-
-; <label>:2:
-  br label %9
-
-; <label>:3:
-  %4 = add nsw i32 %0, -1
-  %5 = tail call i32 @Fib(i32 %4)
-  %6 = add nsw i32 %0, -2
-  %7 = tail call i32 @Fib(i32 %6)
-  %8 = add nsw i32 %7, %5
-  ret i32 %8
-
-; <label>:9:
-  %10 = phi i32 [ 1, %2 ], [ %0, %1 ]
-  ret i32 %10
+int Fib(int x) {
+    switch(x) {
+        case 0:
+            return 0;
+        case 1:
+            return 1;
+        default:
+            return Fib(x - 1) + Fib(x - 2);
+  }
 }
 """
 
@@ -40,9 +27,9 @@ def Explorer(urls):
     "endpoints": ENDPOINTS,
     "defaults": {
       "ir": DEFAULT_IR,
-      "lang": "llvm",
-      "version": "6.0.0",
-      "programl_version": "2020.05.06",
+      "lang": "clang",
+      "version": "default",
+      "programl_version": "default",
     },
   }
   urls["highlight_js"] = flask.url_for("static", filename="highlight.pack.js")
